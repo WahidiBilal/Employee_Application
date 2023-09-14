2c53e76fd6cf0285de0c427e67768f6a79b92d6d
Muhammad Bilal Wahidi <wahidibilal0094@gmail.com>
2023-09-14 21:24:37 +0200

    Changes in EmployeeManagementApp and Add dependencies in POM file.

--- src/main/java/com/bilal/employeeapp/EmployeeManagementApplication.java
+++ src/main/java/com/bilal/employeeapp/EmployeeManagementApplication.java
@@ -2,9 +2,11 @@ package com.bilal.employeeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.bilal.employeeapp")
 public class EmployeeManagementApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/bilal/employeeapp/EmployeeManagementApplicationTest.java
+++ src/test/java/com/bilal/employeeapp/EmployeeManagementApplicationTest.java
@@ -1,34 +1,15 @@
 package com.bilal.employeeapp;
-//
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.junit.jupiter.api.Assertions;
-//
-//
-//@SpringBootTest
-//public class EmployeeManagementApplicationTest {
-//
-//	@Test
-//	public void contextLoads() {
-//		
-//		 Assertions.assertTrue(true, "The context should load successfully.");
-//		
-//	}
-//
-//}
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest(classes = EmployeeManagementApplication.class)
 public class EmployeeManagementApplicationTest {
 
-    @Test
-    public void mainMethodRunsSuccessfully() {
-        // You can call the main method directly and check for any exceptions
-        try {
-            EmployeeManagementApplication.main(new String[]{});
-        } catch (Exception e) {
-            fail("The main method should run without exceptions.");
-        }
-    }
+	@Test
+	public void contextLoads() {
+	}
+
 }
